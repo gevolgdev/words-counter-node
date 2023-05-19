574a1496960bdcee693b30ab62f99ff0540c9dae
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-19 14:27:13 -0300

    Server pronto

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const wordsCounter = require('./wordsCounter');
 
-app.get('/', (req, res) => {
-  res.send('Hello World, devs!');
+app.get('/', async (req, res) => {
+  const wordsCount = await wordsCounter();
+  res.send(`Número de palavras são: ${wordsCount} palavras.`);
 });
 
 app.listen(port, () => {
