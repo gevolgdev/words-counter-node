da84926d2d48c71eb05d1fa0debaf2153566629a
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-19 14:27:27 -0300

    Logica do contator de palavras

---
+++ src/wordsCounter.js
@@ -0,0 +1,19 @@
+const { readFile } = require('node:fs/promises');
+const { resolve } = require('node:path');
+
+const wordsCounter = async () => {
+  try {
+    const filePath = resolve('./texts/javascript-history.txt');
+    const content = await readFile(filePath, {encoding: 'utf-8'});
+    const words = content.split(" ");
+    const wordsCount = words.length;
+    console.log(`Numero de palavras são: ${wordsCount} palavras.`);
+    return wordsCount;
+  }
+  catch (err) {
+    console.log(`Erro ao ler o arquivo!`, err);
+    return 0;
+  }
+};
+
+module.exports = wordsCounter;
